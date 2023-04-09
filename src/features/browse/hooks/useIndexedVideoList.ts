600e40119839aa3e89d7bd8f04f3267c1fbb8f81
domuk-k <dannyworks102@gmail.com>
2023-04-09 17:41:20 +0900

    feat: list all pages of videos in client side

--- src/features/browse/hooks/useIndexedVideoList.ts
+++ src/features/browse/hooks/useIndexedVideoList.ts
@@ -1,16 +1,35 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { ListVideosResponse, PublicAPIError } from '@/pages/api/types';
+import { ListVideosResponse, PublicAPIError, Video } from '@/pages/api/types';
 import fetcher from '@/libs/fetcher';
+import range from '@/utils/range';
 
 interface UseIndexedVideoList {
   indexId: string;
 }
 
+const MAXIUM_PAGE_LIMIT = 50; // To list all the videos, setting page limit as its maximum would reduce api call count
+
 function useIndexedVideoList({ indexId }: UseIndexedVideoList) {
-  const response = useQuery<ListVideosResponse, PublicAPIError>(
+  const response = useQuery<Video[], PublicAPIError>(
     ['index', indexId, 'videos'],
-    () => fetcher<ListVideosResponse>(`api/indexes/${indexId}/videos`),
+    async () => {
+      const result: ListVideosResponse[] = [
+        await fetchVideoList({ indexId, page: 1 }),
+      ];
+
+      const { page_info } = result[0];
+
+      if (page_info.total_page > 1) {
+        const callsForFurtherPages = range(2, page_info.total_page).map(
+          (page) => fetchVideoList({ indexId, page })
+        );
+
+        result.push(...(await Promise.all(callsForFurtherPages)));
+      }
+
+      return result.flatMap((r) => r.data);
+    },
     {
       // TODO: remove this option after upload feature. this assumes no mutations occur
       staleTime: Infinity,
@@ -19,8 +38,13 @@ function useIndexedVideoList({ indexId }: UseIndexedVideoList) {
 
   return {
     ...response,
-    data: response.data?.data ?? [],
+    data: response.data ?? [],
   };
 }
 
 export default useIndexedVideoList;
+
+const fetchVideoList = ({ indexId, page }: { indexId: string; page: number }) =>
+  fetcher<ListVideosResponse>(
+    `api/indexes/${indexId}/videos?page=${page}&page_limit=${MAXIUM_PAGE_LIMIT}`
+  );
